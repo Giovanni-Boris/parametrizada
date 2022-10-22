e9cb5984dc6cf61b9412dba14d4849c1b1508f0e
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-10-22 07:16:57 -0500

    4toaxioma

--- StackArray.cc
+++ StackArray.cc
@@ -1,13 +1,25 @@
 #include "StackArray.h"
 #include <assert.h>
+#include <iostream>
+template<typename T>
+StackArray<T>::StackArray(int s){
+  size = s;
+  data = new T[s];
+  sp = data;
+}
 template<typename T>
 void StackArray<T>::push(T e){
   int numElem = sp - data;
+  //std::cout<<sp<<" : " <<data<<"\n";
   if(numElem == size){
     resize();
   }
   *sp = e;
   sp++;
+  //e = 12;
+  //4 top(push(S,I) = I)
+  std::cout<<"top: "<<top()<<" : e= "<<e<<"\n"; 
+  assert(top()==e);
 }
 template<typename T>
 void StackArray<T>::pop(){
@@ -17,6 +29,8 @@ void StackArray<T>::pop(){
 }
 template<typename T>
 T StackArray<T>::top(){
+  //if the stack is empty
+  //top(newstack) = undefinedº
   assert(!empty());
   return *(sp-1);
 }
@@ -27,18 +41,37 @@ bool StackArray<T>::empty(){
 template<typename T>
 void StackArray<T>::resize(){
   int newSize =size + RESIZEFACTOR;
-  T newData[] = new T[newSize];
+  T* newData= new T[newSize];
   int numElem = sp - data;
+  std::cout<<"numElem: "<< numElem<<"\n";
   for(int i=0;i<size;i++){
     newData[i] = data[i];
   }
-  T oldData[] = data;
+  T* oldData = data;
 
   data = newData;
   size = newSize;
   sp = newData + numElem;
-
+  std::cout<<"Size: "<< size<<"\n";
   delete[] oldData;
+};
+template<typename T>
+void StackArray<T>::print(){
+  int numElem = sp -data;
+  for(int i=0;i<numElem;i++)
+    std::cout<<data[i]<<"--->";
+  std::cout<<"\n";
 }
-
-
+int main(){
+  StackArray<int> c(2);
+  //c.top();
+  c.push(1);
+  c.push(2);
+  c.push(3);
+  c.push(4);
+  c.print();
+  /*std::cout<<"top: "<<c.top()<<"\n";
+  c.pop();
+  c.print();
+  std::cout<<"top: "<<c.top()<<"\n";*/
+}
