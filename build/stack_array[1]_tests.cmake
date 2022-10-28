4aa2fbb99346a7c5db96ec4355f99c1056767903
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-10-28 15:57:08 -0500

    Diferencias entr epxect y assert

--- build/stack_array[1]_tests.cmake
+++ build/stack_array[1]_tests.cmake
@@ -1,3 +1,5 @@
 add_test( Stack.StackEMpty /home/boris/Escritorio/parametrizada/build/stack_array [==[--gtest_filter=Stack.StackEMpty]==] --gtest_also_run_disabled_tests)
 set_tests_properties( Stack.StackEMpty PROPERTIES WORKING_DIRECTORY /home/boris/Escritorio/parametrizada/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set( stack_array_TESTS Stack.StackEMpty)
+add_test( Stack.StackNotEMpty /home/boris/Escritorio/parametrizada/build/stack_array [==[--gtest_filter=Stack.StackNotEMpty]==] --gtest_also_run_disabled_tests)
+set_tests_properties( Stack.StackNotEMpty PROPERTIES WORKING_DIRECTORY /home/boris/Escritorio/parametrizada/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set( stack_array_TESTS Stack.StackEMpty Stack.StackNotEMpty)
