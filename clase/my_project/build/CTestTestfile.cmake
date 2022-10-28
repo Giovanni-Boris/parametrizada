6da5c1a28cd6c0d445d29fcae21167096a03168a
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-10-28 14:36:44 -0500

    Ejercicio calse

---
+++ clase/my_project/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/boris/Escritorio/parametrizada/clase/my_project
+# Build directory: /home/boris/Escritorio/parametrizada/clase/my_project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/boris/Escritorio/parametrizada/clase/my_project/build/hello_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
---
+++ clase/my_project/build/hello_test[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/home/boris/Escritorio/parametrizada/clase/my_project/build/hello_test[1]_tests.cmake")
+  include("/home/boris/Escritorio/parametrizada/clase/my_project/build/hello_test[1]_tests.cmake")
+else()
+  add_test(hello_test_NOT_BUILT hello_test_NOT_BUILT)
+endif()
